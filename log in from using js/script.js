9fe4f855493b0f9d5a8c4b39f0abac2af8c7cf6a
Shrinivas Kulkarni <118358705+kulkarnishri19@users.noreply.github.com>
2023-05-25 12:25:01 +0530

    Add files via upload

---
+++ log in from using js/script.js
@@ -0,0 +1,29 @@
+const froms = document.querySelector(".forms"),
+      pwShowHide = document.querySelectorAll(".eye-icon"),
+      link = document.querySelectorAll(".link");
+
+pwShowHide.forEach(eyeIcon => 
+    {
+        eyeIcon.addEventListener("click",() => {
+         let pwShowHide = eyeIcon.parentElement.parentElement.querySelector(".password");
+       pwFields.forEach(password  => {
+            if(password.type === "password"){
+                password.type ="text";
+                eyeIcon.classList.replace("bx-hide","bx-show");
+                return;
+
+            }
+            password.type ="password";
+            eyeIcon.classList.replace("bx-show","bx-hide");
+        })
+        })
+    })
+    link.forEach(link => {
+        link.addEventListener ("click",e=> {
+            e.preventDefault();
+         froms.classList.toggle("Show-signup");
+
+        })
+    
+ } )
+
